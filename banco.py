e31453b017675133b702062d2cc574384600fa9e
hugopierry <hugopierryapc16@gmail.com>
2026-08-26 11:27:16 -0300

    adiciona tabela de produtos e inicia migração do armazenamento em RAM para SQLite

--- banco.py
+++ banco.py
@@ -1,31 +1,42 @@
-import sqlite3  
-
+import sqlite3
+# importa o sql
 
 
 def conectar():
+# Define a função de conexão
     conexao = sqlite3.connect("mini_wms.db")
+    # conexão que integra ao banco de dados
     return conexao
-            
+    # após isso, é retornado a conexão.
+
 def criar_tabelas():
+# função para criar a tabela
     conexao = conectar()
+    # define a conexão e inicia
     cursor = conexao.cursor()
+    # cria o cursor para executar comandos SQL
 
-   
     comando_sql = """
-    CREATE TABLE IF NOT EXISTS usuarios(
-        matricula TEXT PRIMARY KEY,
-        senha TEXT,
-        nome TEXT
-    );
+        CREATE TABLE IF NOT EXISTS produtos(
+            id INTEGER PRIMARY KEY,
+            codigo_barras TEXT,
+            sku TEXT,
+            descricao TEXT,
+            caixaria INTEGER,
+            validade TEXT,
+            lote TEXT,
+            quantidade INTEGER,
+            valor_unitario REAL
 
-"""
-    cursor.execute(comando_sql)
+
+        
+        
+        );
     
+    """
+    cursor.execute(comando_sql)
     conexao.commit()
+    print("Banco de dados para Produtos criado com sucesso!")
     conexao.close()
-import os
 
-def conectar():
-    conexao = sqlite3.connect("mini_wms.db")
-    print("Caminho do banco:", os.path.abspath("mini_wms.db"))
-    return conexao
+criar_tabelas()
