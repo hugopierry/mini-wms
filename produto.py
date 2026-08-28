b9ce9bf26e93adb80b97a24dafe109f433399b19
hugopierry <hugopierryapc16@gmail.com>
2026-08-28 11:10:09 -0300

    Revisão e comentários no produto.py

--- produto.py
+++ produto.py
@@ -5,10 +5,17 @@ class Produto():
         self.quantidade = quantidade
         self.valor_unitario = valor_unitario
         self.total = valor_unitario * quantidade
+        # Essa variável recebe o valor unitário e multiplica pela quantidade, apresentando o total do item.
+        # Será revisto, e terá essa informação atualzada.
     
     def exibir_produto(self):
         print(f"Código: {self.codigo}\nDescrição: {self.descricao}\nQuantidade: {self.quantidade}\nValor Unitário: R${self.valor_unitario:.2f}\nTotal: R${self.total:.2f} ")
 
+    """
+    A classe Produto representa os dados de cada produto.
+    A função exibir_produto() apresenta esses dados
+    de forma organizada.
+    """
 
 
 
