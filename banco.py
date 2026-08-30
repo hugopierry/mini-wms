a8e156c6fdd66b005cf3819623018bb82790816d
hugopierry <hugopierryapc16@gmail.com>
2026-08-30 17:27:34 -0300

    Integra operações de INSERT, SELECT e DELETE ao SQLite

--- banco.py
+++ banco.py
@@ -38,21 +38,51 @@ def criar_tabelas():
 
     cursor.execute(comando_sql)
     # Executa o comando SQL criado acima
-
-
-        
-        
-    comanod_select = """
+       
+    comando_select = """
         SELECT  *  FROM produtos;
 
 """
-    cursor.execute(comanod_select)
+    cursor.execute(comando_select)
     dados_exibir = cursor.fetchall()
-    print(dados_exibir)
+    # Comando que traz os dados de forma organizada ao exibir
+    print("=" * 130)
+    print("RELATÓRIO ITENS CADASTRADOS".center(50))
+    print("=" * 130)
+    print(
+         f"{'ID':<10}"
+    f"{'CÓDIGO DE BARRAS':<20}"
+    f"{'SKU'}"
+    f"{'DESCRIÇÃO':<15}"
+    f"{'CAIXARIA':<15}"
+    f"{'VALIDADE':<15}"
+    f"{'LOTE':<15}"
+    f"{'QUANTIDADE':<15}"
+    f"{'VALOR UNITÁRIO':<15}"
+
+
+    )
+    # Cria as informações que serão os títulos das colunas
+    print("-" * 130)
+
+    for p in dados_exibir:
+    # Percorre cada registro retornado pelo fetchall()
+    # e imprime o relatório
+        print(
+        f"{p[0]:<10}"
+        f"{p[1]:<20}"
+        f"{p[2]}"
+        f"{p[3]:<15}"
+        f"{p[4]:<15}"
+        f"{p[5]:<15}"
+        f"{p[6]:<15}"
+        f"{p[7]:<15}"
+        f"{p[8]:<15}"
+            
+        )
+       
     conexao.commit()
-    # Confirma e registra a alteração realizada no banco
-    
-    
+   
     # Exibe uma mensagem de confirmação
 
     conexao.close()
@@ -90,14 +120,29 @@ def inserir_produto(codigo_barras, sku, descricao, caixaria,
     conexao.commit()
     conexao.close()
 criar_tabelas()
-inserir_produto('789000000020',
-    'SKU020',
-    'Mouse',
-    12,
-    '2032/09/10',
-    'LOTE020',
+inserir_produto('789000000021',
+    'SKU021',
+    'Headphone',
     15,
-    35.90)
+    '2032/09/10',
+    'LOTE021',
+    15,
+    186.90)
+
 
 # Chama a função para executar o processo
 
+def deletar_item():
+    conexao = conectar()
+        
+    cursor = conexao.cursor()
+    # Comando criado para apagar um item via ID
+    # que estava gerando erro no código
+    comando_deletar = """
+    DELETE FROM produtos
+        WHERE id = 1
+    """
+    cursor.execute(comando_deletar)
+    conexao.commit()
+    conexao.close()
+deletar_item()
