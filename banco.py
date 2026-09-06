4f201c52653df437c9ecd46938a802e3f7ad1561
hugopierry <hugopierryapc16@gmail.com>
2026-09-06 12:41:06 -0300

    Remove código temporário de consulta de usuários

--- banco.py
+++ banco.py
@@ -272,15 +272,5 @@ def buscar_produtos():
 # cadastrar_item()
 # atualizar_item()
 
-conexao = conectar()
-cursor = conexao.cursor()
-
-cursor.execute("SELECT matricula, senha FROM usuarios")
-
-usuarios = cursor.fetchall()
-
-for usuario in  usuarios:
-    print(usuario)
-
 # SQLite: "database is locked" geralmente pode ser porque o banco de dados está em outra
 # conexão. Fechando o banco, o acesso é liberado.
